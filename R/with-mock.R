529b3067b0e597412cdc5deaadd1a14d9f729e7f
Kirill Müller <krlmlr+r@mailbox.org>
2016-12-23 22:59:47 +0100

    simplify

--- R/with-mock.R
+++ R/with-mock.R
@@ -107,20 +107,28 @@ extract_mocks <- function(dots, env) {
 }
 
 extract_mock <- function(qual_name, dot, env) {
+  name <- extract_mock_name(qual_name)
+  check_mock(name, env)
+  mock(name = name, new = lazyeval::lazy_eval(dot))
+}
+
+extract_mock_name <- function(qual_name) {
   pkg_rx <- ".*[^:]"
   colons_rx <- "::(?:[:]?)"
   name_rx <- ".*"
   pkg_and_name_rx <- sprintf("^(?:(%s)%s)?(%s)$", pkg_rx, colons_rx, name_rx)
 
   pkg_name <- gsub(pkg_and_name_rx, "\\1", qual_name)
-
-  name <- gsub(pkg_and_name_rx, "\\2", qual_name)
-
   if (pkg_name != "") {
     warning("with_mock() cannot mock functions defined in other packages.",
             call. = FALSE)
   }
 
+  name <- gsub(pkg_and_name_rx, "\\2", qual_name)
+  name
+}
+
+check_mock <- function(name, env) {
   orig <- mget(name, envir = env, ifnotfound = list(NULL))[[1]]
   if (is.null(orig)) {
     stop(name, " not found in environment ",
@@ -130,11 +138,8 @@ extract_mock <- function(qual_name, dot, env) {
     stop(name, " is not a function in environment ",
          environmentName(env), ".", call. = FALSE)
   }
-  mock(name = name, env = env, orig = orig, new = lazyeval::lazy_eval(dot))
 }
 
 mock <- function(name, env, orig, new) {
-  structure(list(
-    env = env, name = as.name(name),
-    orig_value = orig, new_value = new), class = "mock")
+  structure(list(name = as.name(name), new_value = new), class = "mock")
 }
