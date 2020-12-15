515cc91e41850f66764f1ce4e7220d85b444e8b1
Kirill Müller <krlmlr+r@mailbox.org>
2020-12-15 07:32:07 +0100

    Fix test

--- R/test.R
+++ R/test.R
@@ -2,7 +2,7 @@ some_symbol <- 42
 
 mocker <- function() mockee()
 
-.mocker <- function() mockee()
+.mocker <- function() .mockee()
 
 mockee <- function() stop("Not mocking")
 
@@ -10,4 +10,4 @@ mockee2 <- function() stop("Not mocking (2)")
 
 mockee3 <- function() mockee()
 
-.mockee <- function() stop("Not mocking")
+.mockee <- function() stop("Not mocking (3)")
