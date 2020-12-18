3f257db4efbeadf6bf507826ce249a3f7ed2e0e5
Kirill Müller <krlmlr+r@mailbox.org>
2020-12-18 14:30:59 +0100

    Extract local_mock_env()

---
+++ R/local.R
@@ -0,0 +1,5 @@
+local_mock_env <- function(mock_env, .parent, env = parent.frame()) {
+  old_parent <- parent.env(.parent)
+  withr::defer(parent.env(.parent) <- old_parent, env)
+  parent.env(.parent) <- mock_env
+}
